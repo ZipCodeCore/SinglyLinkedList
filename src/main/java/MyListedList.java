eec3953c78501eaa95f5d13ed752d5931ff1f0e4
Randall Crame <randallcrame@zipcoders-MacBook-Pro.local>
2017-02-24 16:13:34 -0500

    improved stub

--- src/main/java/MyListedList.java
+++ src/main/java/MyListedList.java
@@ -3,16 +3,30 @@
  */
 public class MyListedList <T> {
 
-    public void add(){}
-    public void remove(){}
-    public boolean contains(){return false;};
-    public int find(){return 0;}
+    private Node head;
+    private int modList;
+
+    public void add(T data){}
+    public void remove(int index){}
+    public boolean contains(T data){return false;};
+    public int find(T data){return 0;}
     public int size(){return 0;}
-    public T get(){return null;}
-    public T copy(){return null;}
+    public T get(int index){return null;}
+    public MyListedList<T> copy(){return null;}
     public void sort(){}
 
-    private class node{
+    private static class Node <T> {
+        private T data;
+        private Node next;
 
+        public Node(T data){
+            this.data = data;
+            this.next = null;
+        }
+
+        public Node(T data, Node<T> next){
+            this.data = data;
+            this.next = next;
+        }
     }
 }
