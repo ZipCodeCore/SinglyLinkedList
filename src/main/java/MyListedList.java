268467fd98db4e93a0571884b94daba2dce55d56
Randall Crame <randallcrame@zipcoders-MacBook-Pro.local>
2017-02-24 15:15:28 -0500

    stubbed out methods

---
+++ src/main/java/MyListedList.java
@@ -0,0 +1,18 @@
+/**
+ * Created by randallcrame on 2/24/17.
+ */
+public class MyListedList <T> {
+
+    public void add(){}
+    public void remove(){}
+    public boolean contains(){return false;};
+    public int find(){return 0;}
+    public int size(){return 0;}
+    public T get(){return null;}
+    public T copy(){return null;}
+    public void sort(){}
+
+    private class node{
+
+    }
+}
