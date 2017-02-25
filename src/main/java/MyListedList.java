fc6ca9b5abb40e1c0d1e63038f9fecdb8da3e7e5
Randall Crame <randallcrame@zipcoders-MacBook-Pro.local>
2017-02-24 20:20:46 -0500

    Worked on logic and tests up to sort()

--- src/main/java/MyListedList.java
+++ src/main/java/MyListedList.java
@@ -4,16 +4,95 @@
 public class MyListedList <T> {
 
     private Node head;
+    private Node tail;
     private int modList;
 
-    public void add(T data){}
-    public void remove(int index){}
-    public boolean contains(T data){return false;};
-    public int find(T data){return 0;}
-    public int size(){return 0;}
-    public T get(int index){return null;}
-    public MyListedList<T> copy(){return null;}
-    public void sort(){}
+    MyListedList() {
+        this.head = new Node(null);
+        this.tail = this.head;
+        this.modList = 0;
+    }
+
+    public void add(T data){
+        Node newNode = new Node(data);
+        Node formerTail = this.tail;
+        if (modList == 0)
+            head = newNode;
+        else
+            formerTail.setNext(newNode);
+        tail = newNode;
+        modList++;
+    }
+
+    public void remove(int index){
+        if (index == 0) {
+            head = head.getNext();
+        }else {
+            int counter = 1;
+            Node<T> current = head;
+            while (index < counter){
+                current = current.getNext();
+                counter++;
+            }
+            if (counter == modList)
+                tail = current;
+            current.setNext(current.getNext().getNext());
+        }
+        modList--;
+    }
+
+    public boolean contains(T data){
+        return !(find(data) == -1);
+    }
+
+    public int find(T data){
+        Node<T> current = head;
+        Node<T> previous;
+        int counter = 0;
+        do{
+            if (current.getData().equals(data))
+                return counter;
+            previous = current;
+            current = current.getNext();
+            counter++;
+        }while (previous.getNext() != null);
+        return -1;
+    }
+
+    public int size(){return modList;}
+
+    public T get(int index) throws NullPointerException{
+        int counter = 0;
+        if (index > modList)
+            throw new NullPointerException();
+        Node<T> current = head;
+
+            while (counter != index){
+                current = current.getNext();
+                counter++;
+            }
+        try {
+            return current.getData();
+        } catch (NullPointerException e){
+            return null;
+        }
+    }
+
+    public MyListedList<T> copy(){
+        MyListedList<T> copy = new MyListedList<>();
+        Node<T> current = head;
+        Node<T> previous;
+        do{
+            copy.add(current.getData());
+            previous = current;
+            current = current.getNext();
+        }while (previous.getNext() != null);
+        return copy;
+    }
+
+    public void sort(){
+
+    }
 
     private static class Node <T> {
         private T data;
@@ -24,8 +103,15 @@ public class MyListedList <T> {
             this.next = null;
         }
 
-        public Node(T data, Node<T> next){
-            this.data = data;
+        public T getData() {
+            return this.data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
             this.next = next;
         }
     }
---
+++ src/test/java/MyListedListTest.java
@@ -0,0 +1,144 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by randallcrame on 2/24/17.
+ */
+public class MyListedListTest {
+    MyListedList<String> test, test2;
+    String item1, item2, item3;
+
+
+    @Before
+    public void setUp() {
+        item1 = "First";
+        item2 = "Second";
+        item3 = "Third";
+        test = new MyListedList<>();
+        test2 = new MyListedList<>();
+        test2.add(item1);
+        test2.add(item2);
+        test2.add(item3);
+
+    }
+
+    @Test
+    public void addTest() {
+        test.add(item1);
+        int expected = 1;
+        int actual = test.size();
+        assertEquals("Expected return of 1", expected, actual);
+    }
+
+    @Test
+    public void add2Test() {
+        test.add(item1);
+        test.add(item2);
+        int expected = 2;
+        int actual = test.size();
+        assertEquals("Expected return of 2", expected, actual);
+    }
+
+    @Test
+    public void get0Test() {
+        String expected = "First";
+        String actual = test2.get(0);
+        assertEquals("Expected return of 'First'", expected, actual);
+    }
+
+    @Test
+    public void get1Test() {
+        String expected = "Second";
+        String actual = test2.get(1);
+        assertEquals("Expected return of 'Second'", expected, actual);
+    }
+
+    @Test
+    public void get2Test() {
+        String expected = "Third";
+        String actual = test2.get(2);
+        assertEquals("Expected return of 'Third'", expected, actual);
+    }
+
+    @Test
+    public void get3Test() {
+        String actual = test2.get(3);
+        assertNull("Expected return of 'null'", actual);
+    }
+
+    @Test
+    public void removeTest() {
+        test2.remove(0);
+        int expected = 2;
+        int actual = test2.size();
+        assertEquals("Expected return of 2", expected, actual);
+
+    }
+
+    @Test
+    public void removeHeadTest() {
+        test2.remove(0);
+        String expected = "Second";
+        String actual = test2.get(0);
+        assertEquals("Expected return of Second", expected, actual);
+    }
+
+    @Test
+    public void removeMiddleTest() {
+        test2.remove(1);
+        String expected = "Third";
+        String actual = test2.get(1);
+        assertEquals("Expected return of Third", expected, actual);
+    }
+
+    @Test
+    public void removeTailTest() {
+        test2.remove(2);
+        test2.add(item1);
+        String expected = "First";
+        String actual = test2.get(2);
+        assertEquals("Expected return of First", expected, actual);
+    }
+
+    @Test
+    public void containsTrueTest() {
+        boolean actual = test2.contains("Second");
+        assertTrue("Expected return True", actual   );
+    }
+
+    @Test
+    public void containFalseTest() {
+        boolean actual = test2.contains("11");
+        assertFalse("Expected return False", actual);
+    }
+
+    @Test
+    public void findPositiveTest(){
+        int expected = 2;
+        int actual = test2.find("Third");
+        assertEquals("Expected return of 2", expected, actual);
+    }
+
+    @Test
+    public void findNegativeTest(){
+        int expected = -1;
+        int actual = test2.find("11");
+        assertEquals("Expected return of -1", expected, actual);
+    }
+
+    @Test
+    public void copyTest(){
+        int expected = 3;
+        int actual = test2.copy().size();
+        assertEquals("Expected return of 3", expected, actual);
+    }
+
+    @Test
+    public void copyHeadTest(){
+        String expected = "First";
+        String actual = test2.copy().get(0);
+        assertEquals("Expected return of First", expected, actual);
+    }
+}
