c1304f43f182892d2a81e28ffb0aebebc58ec7de
elliott <elliott@zipcoders-MacBook-Pro.local>
2017-11-16 19:31:33 -0500

    finishedMainReqs

---
+++ src/main/java/LinkedList.java
@@ -0,0 +1,200 @@
+import sun.awt.image.ImageWatched;
+
+import java.util.Comparator;
+import java.util.Iterator;
+
+public class LinkedList<T extends Comparable<T>> {
+
+    Node head;
+    Node tail;
+
+    public void add(T value) {
+        if (head == null) {
+            head = new Node(value);
+            tail = head;
+        } else {
+            tail.next = new Node(value);
+            tail = tail.next;
+        }
+    }
+
+    public T removeByValue(T val) {
+        Node nxt = head;
+        T rem = null;
+        if (head.value.equals(val)) {
+            rem = head.value;
+            head = head.next;
+
+        } else
+            while (nxt.next != null) {
+                if (nxt.next.value.equals(val)) {
+                    rem = nxt.next.value;
+                    if (nxt.next.next != null)
+                        nxt.next = nxt.next.next;
+                    else {
+                        tail = nxt;
+                        nxt.next = null;
+                    }
+                    break;
+                }
+                nxt = nxt.next;
+            }
+
+        return rem;
+    }
+
+    public Node remove(int index) {
+        try {
+            Node rem = null;
+            if (index == 0) {
+                rem = head;
+                head = head.next;
+            } else {
+                int count = 1;
+                Node remove = head;
+                while (count != index) {
+                    remove = remove.next;
+                    count++;
+                }
+                if (remove.next.next != null) {
+                    rem = remove.next;
+                    remove.next = remove.next.next;
+                } else {
+                    rem = tail;
+                    tail = remove;
+                    remove.next = null;
+                }
+                return rem;
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Invalid Index");
+        }
+        return null;
+    }
+
+
+    public boolean contains(T val) {
+        if (find(val) != -1)
+            return true;
+        else return false;
+
+    }
+
+    public int find(T val) {
+        Node nxt = head;
+        int count = 0;
+        if (head.value.equals(val)) {
+            return count;
+
+        } else
+            while (nxt.next != null) {
+                count++;
+                if (nxt.next.value.equals(val))
+                    return count;
+
+                nxt = nxt.next;
+            }
+        return -1;
+    }
+
+    public int size() {
+        int count = 1;
+        Node nxt = head;
+        while (nxt.next != null) {
+            count++;
+            nxt = nxt.next;
+        }
+
+        return count;
+    }
+
+    public T get(int index) {
+        try {
+            if (index == 0) {
+                return head.value;
+            } else {
+                int count = 1;
+                Node item = head;
+                while (count != index) {
+                    item = item.next;
+                    count++;
+                }
+                return item.next.value;
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Invalid Index");
+        }
+        return null;
+    }
+
+
+    public LinkedList<T> copy() {
+        LinkedList<T> copy = new LinkedList<T>();
+        for (int i = 0; i < this.size(); i++)
+            copy.add(this.get(i));
+
+        return copy;
+    }
+
+    public void insert(int index, T value) {
+        Node newNode = new Node(value);
+        if (index == 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            int count = 1;
+            Node item = head;
+            while (count != index) {
+                item = item.next;
+                count++;
+            }
+            newNode.next = item.next;
+            item.next = newNode;
+        }
+
+    }
+
+    public void sort() {
+        for (int i = 1; i < size(); i++) {
+            Node temp = remove(i);
+            for (int j = i - 1; j >= 0; j--) {
+                if (j == 0) {
+                    insert((get(j).compareTo(temp.value) == -1 ? 1 : 0), temp.value);
+                    break;
+                } else if (get(j).compareTo(temp.value) < 0) {
+                    insert(j + 1, temp.value);
+                    break;
+                } else if (get(j).compareTo(temp.value) == 0) {
+                    insert(j, temp.value);
+                    break;
+                }
+
+            }
+        }
+
+    }
+
+    public String toString() {
+        Node nxt = head;
+        String list = "";
+        while (nxt.next != null) {
+            list += nxt.value;
+            nxt = nxt.next;
+        }
+        list += nxt.value;
+        return list;
+    }
+
+
+    class Node {
+
+        private Node next;
+        private T value;
+
+        Node(T value) {
+            this.value = value;
+        }
+
+
+    }
+}
---
+++ src/test/java/LinkedListTest.java
@@ -0,0 +1,143 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import sun.awt.image.ImageWatched;
+
+import static org.junit.Assert.*;
+
+
+public class LinkedListTest {
+    LinkedList<String> list=new LinkedList();
+
+    @Test
+    public void addTest() throws Exception {
+        String expected ="345";
+
+        list.add("3");
+        list.add("4");
+        list.add("5");
+        String actual =list.toString();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeTest() throws Exception {
+
+        String expected="abc";
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+
+
+        list.remove(3);
+        String actual=list.toString();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeByValueTest() throws Exception {
+
+        String expected="abd";
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+
+        list.removeByValue("c");
+        String actual=list.toString();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void containsTest() throws Exception {
+        boolean expected=false;
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        boolean actual=list.contains("e");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findTest() throws Exception {
+        int expected=2;
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        int actual=list.find("c");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sizeTest() throws Exception {
+
+        int expected=4;
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        int actual=list.size();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getTest() throws Exception {
+        String expected="c";
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        String actual=list.get(2);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void copyTest() throws Exception {
+        String expected="abcd";
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        LinkedList copy=list.copy();
+        String actual=copy.toString();
+       Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void insertTest() throws Exception {
+        String expected="abzcd";
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.insert(4,"z");
+        String actual=list.toString();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void sortTest() throws Exception {
+        String expected="aabcdez";
+
+        list.add("e");
+        list.add("c");
+        list.add("b");
+        list.add("a");
+        list.add("d");
+        list.add("a");
+        list.insert(4,"z");
+
+        list.sort();
+        String actual=list.toString();
+        Assert.assertEquals(expected, actual);
+    }
+
+
+}
