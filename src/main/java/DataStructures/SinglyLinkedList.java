4ed5b6ce875664684412c36823cbd18cdc3c6080
timothy <timothy@zipcoders-MacBook-Pro.local>
2017-11-17 10:13:44 -0500

    SinglyLinkedList and Tests

--- src/main/java/DataStructures/SinglyLinkedList.java
+++ src/main/java/DataStructures/SinglyLinkedList.java
@@ -1,8 +1,8 @@
 package DataStructures;
 
-public class SinglyLinkedList<E> {
+public class SinglyLinkedList<E extends Comparable> {
 
-    private class Node<E>{
+    private class Node{
         private int index;
         private E element;
         Node next=null;
@@ -14,49 +14,251 @@ public class SinglyLinkedList<E> {
             this.index=index;
             this.element=element;
         }
+
     }//No need for setters and getters: Outer class can access directly.
 
-    private Node head=null;
+    private Node head;
+
+    public SinglyLinkedList(){
+        head=null;
+    }
+
+    private Node findLastNode(){
+        if (head==null){
+            return null;
+        }
+        Node traverse = head;
+        while (traverse.next!=null){
+            traverse=traverse.next;
+        }
+        return traverse;
+    }
 
     public void add (E element){
 
+        Node node = new Node();
+
+        node.element=element;
+        node.next=null;
+
+        if (head==null){
+            head=node;
+            head.index=0;
+        }
+        else{
+            node.index=findLastNode().index+1;
+            findLastNode().next=node;
+        }
+
+    }
+
+    private boolean isIndexOutOfBounds(int index){
+        return (index<0 || index>=size());
+    }
+
+    private void adjustIndices(){
+
+        if (head!=null) {
+
+            int index = 0;
+            Node traverse = head;
+
+            traverse.index = index;
+
+            while (traverse.next != null) {
+                index++;
+                traverse.next.index = index;
+                traverse = traverse.next;
+            }
+        }
     }
 
     public void remove(int index){
+        adjustIndices();
+        if (!(isIndexOutOfBounds(index))) {
+            if (index==0) {
+                head = head.next;
+            }
+            else {
+                Node traverse = head;
+                while (traverse.next.index != index) {
+                    traverse = traverse.next;
+                }
+                traverse.next=traverse.next.next;
+            }
+        }
+        adjustIndices();
 
     }
-
-    public void remove(E element){
-
-    }
+//WHAT HAPPENS IF I PASS AN INTEGER OBJECT?
+// deprecated until I can figure this out
+//    public void remove(E element){
+//
+//        if (contains(element))
+//        {
+//            do{
+//                remove(find(element));
+//            }while (contains(element));
+//            adjustIndices();
+//        }
+//    }
 
     public boolean contains(E element){
+
+        if (head==null) {
+            return false;
+        } else {
+            Node traverse = head;
+            do {
+                if (traverse.element.equals(element))
+                    return true;
+                traverse = traverse.next;
+            } while (traverse != null);
+        }
         return false;
     }
 
     public int find(E element){
 
-        return -2;
+        if (contains(element)) {
+
+            Node traverse = head;
+
+            while (traverse != null) {
+                if (traverse.element.equals(element)) {
+                    return traverse.index;
+                }
+                traverse = traverse.next;
+            }
+        }
+        return -1;
     }
 
     public int size(){
-        return -2;
+        if (head==null)
+            return 0;
+        return findLastNode().index+1;
     }
 
     public E get(int index){
-        return null;
+
+        if (isIndexOutOfBounds(index))
+            return null;
+
+        Node traverse = head;
+        while (traverse.index != index){
+            traverse=traverse.next;
+        }
+
+        return traverse.element;
+
     }
 
-    public SinglyLinkedList<E> copy(){
-        return null;
+    public SinglyLinkedList<E> copy(){//deep copy
+
+        SinglyLinkedList<E> copyList = new SinglyLinkedList<>();
+
+        if (head==null){
+            return copyList;
+        }
+
+        Node traverse = head;
+        do {
+            copyList.add(traverse.element);
+            traverse = traverse.next;
+        }while (traverse!=null);
+
+        return copyList;
     }
 
     public void sort(){
 
+        if (size()>2){
+
+            boolean isInOrder;
+            Node traverse;
+            Node swap;
+
+            do{
+                isInOrder=true;
+                traverse = head;
+                swap = traverse.next;
+
+                if (traverse.element.compareTo(swap.element)>0)
+                {
+                    isInOrder=false;
+                    traverse.next=swap.next;
+                    swap.next=traverse;
+                    head=swap;
+                }
+
+                traverse = head;
+                swap = traverse.next;
+
+                while (swap!=null && swap.next!=null) {
+                    if (swap.element.compareTo(swap.next.element) > 0) {//We want to sort in ascending order,
+                        // so if this returns a positive, we need to switch them. Otherwise, we leave them alone.
+                        //This methodology is NOT efficient: imagine a list 1, 2, 3, ... , n-2, n-1, 5, n. This would
+                        //result in us swapping two nodes, returning to the beginning, and doing it all over again until
+                        //it was sorted. This becomes less efficient with a largely unordered list. Plans to refactor later.
+                        isInOrder=false;
+                        Node bubble = swap.next;
+                        swap.next = bubble.next;
+                        bubble.next = swap;
+                        traverse.next = bubble;
+                    }
+                    traverse = traverse.next;
+                    swap=traverse.next;
+                }
+
+            }while (!isInOrder);
+        } else if(size()>1){
+
+            Node traverse = head;
+            Node swap = traverse.next;
+
+            if (traverse.element.compareTo(swap.element)>0)
+            {
+                traverse.next=swap.next;
+                swap.next=traverse;
+                head=swap;
+            }
+        }
+        adjustIndices();
     }
 
-    public SinglyLinkedList<E> slice(int startIndex, int endIndexExclusive){
-        return null;
+    public void reverse(){
+        if (size()>1) {
+            SinglyLinkedList<E> reversed = new SinglyLinkedList<>();
+            int reverseIndex = size() - 1;
+
+            do {
+                reversed.add(get(reverseIndex));
+                reverseIndex--;
+            }while(reverseIndex>=0);
+            head=reversed.head;
+        }
+    }
+
+    public SinglyLinkedList<E> slice(int startIndex, int endIndexExclusive){//returns a copy of the subList indicated,
+                                                                            //or an empty list if arguments are invalid
+
+        SinglyLinkedList<E> copyList = new SinglyLinkedList<>();
+
+        if (isIndexOutOfBounds(startIndex) || isIndexOutOfBounds(endIndexExclusive-1) ||
+            endIndexExclusive<=startIndex  || (endIndexExclusive-startIndex-1)>=size()){
+            return copyList;
+        }
+
+        for (int i=startIndex; i<endIndexExclusive; i++) {
+            if (get(i)==null){
+                return copyList;
+            }
+            copyList.add(get(i));
+        }
+
+        return copyList;
+
     }
 
 }
--- src/test/java/DataStructures/SinglyLinkedListTest.java
+++ src/test/java/DataStructures/SinglyLinkedListTest.java
@@ -3,8 +3,219 @@ package DataStructures;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+
 public class SinglyLinkedListTest {
 
+    @Test
+    public void testAddAndSize(){
 
+        SinglyLinkedList<Integer> intSLL = new SinglyLinkedList<>();
+        intSLL.add(5);
+        int expected = 1;
+        int actual = intSLL.size();
+
+        Assert.assertEquals(expected, actual);
+
+        expected+=3;
+        intSLL.add(4);
+        intSLL.add(4);
+        intSLL.add(4);
+        actual=intSLL.size();
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testRemoveAndGet(){
+
+        SinglyLinkedList<String> strSLL = new SinglyLinkedList<>();
+        int size = strSLL.size();
+        for (int i=0; i<10; i++)
+        {
+            strSLL.add(""+i);
+        }
+        size = strSLL.size();
+        String expected = ""+1;
+
+        for (int i=0; i<5; i++)//Test remove by index
+        {
+            strSLL.remove(0);
+            size = strSLL.size();
+            Assert.assertEquals(expected.compareTo(strSLL.get(0)),0);
+            expected=""+(i+2);
+
+        }
+
+//        for (int i=5; i<10; i++){//Test remove by element overload
+//            strSLL.remove(""+i);
+//            String actual=strSLL.get(0);
+//            if (i!=9) {
+//                Assert.assertEquals(expected.compareTo(strSLL.get(0)), 0);
+//            } else{
+//                Assert.assertNull(strSLL.get(0));
+//            }
+//            expected=""+(i+2);
+//        }
+
+    }
+
+    @Test
+    public void testContains(){
+
+        SinglyLinkedList<String> strSLL = new SinglyLinkedList<>();
+
+        for (int i=0; i<10; i++){
+            strSLL.add(""+i);
+        }
+
+        boolean expected=true;
+        boolean actual;
+
+        for (int i=0; i<10; i++){
+            actual=strSLL.contains(""+i);
+            Assert.assertEquals(expected, actual);
+        }
+
+        expected = false;
+
+        for (int i=10; i<15; i++){
+            actual = strSLL.contains(""+i);
+            Assert.assertEquals(expected, actual);
+        }
+
+    }
+
+    @Test
+    public void testFind(){
+
+        SinglyLinkedList<Integer> intSLL = new SinglyLinkedList<>();
+
+        for (int i=0; i<50; i++){
+            intSLL.add(i);
+        }
+
+        int actual;
+
+        for (int i=49; i>=0; i--){
+            actual = intSLL.find(i);
+            Assert.assertEquals(i, actual);
+        }
+
+
+    }
+
+    @Test
+    public void testCopy(){
+
+        SinglyLinkedList<String> original = new SinglyLinkedList<>();
+
+        for (char letter='a'; letter<='z'; letter++){
+            original.add(""+letter);
+        }
+
+        SinglyLinkedList<String> copy = original.copy();
+
+        for (int i=0; i<26; i++){
+            Assert.assertEquals(original.get(i), copy.get(i));
+        }
+
+        original.remove(0);
+
+        for (int i=0; i<25; i++) {
+            Assert.assertNotEquals(original.get(i), copy.get(i));
+        }
+    }
+
+    @Test
+    public void testSortAndReverse(){
+
+        SinglyLinkedList<Integer> intSLL = new SinglyLinkedList<>();
+        ArrayList<Integer> intAL = new ArrayList<>();
+        Random rando = new Random();
+        int nextInt=rando.nextInt();
+
+        for (int i=0; i<50; i++){
+            intAL.add(nextInt);
+            intSLL.add(nextInt);
+            nextInt=rando.nextInt();
+        }
+
+        int al;
+        int sll;
+
+        for (int i=0; i<50; i++){
+            al=intAL.get(i);
+            sll=intSLL.get(i);
+            Assert.assertEquals(al, sll);
+        }
+
+        Collections.sort(intAL);
+        intSLL.sort();
+
+        for (int i=0; i<50; i++){
+            al=intAL.get(i);
+            sll=intSLL.get(i);
+            Assert.assertEquals(al, sll);
+        }
+
+        Collections.reverse(intAL);
+        intSLL.reverse();
+
+        for (int i=0; i<50; i++){
+            al=intAL.get(i);
+            sll=intSLL.get(i);
+            Assert.assertEquals(al, sll);
+        }
+    }
+
+    @Test
+    public void testSlice(){
+
+        SinglyLinkedList<Integer> intSLL = new SinglyLinkedList<>();
+
+        for (int i=0; i<10; i++){
+            intSLL.add(i);
+        }
+
+        SinglyLinkedList<Integer> sliced = intSLL.slice(2,8);
+        Assert.assertEquals(6, sliced.size());
+        for (int i=0; i<6; i++){
+            Assert.assertEquals(sliced.get(i), intSLL.get(i+2));
+        }
+
+        for (int i=0; i<10; i++){
+            intSLL.remove(i);
+            intSLL.add(i-1);
+        }
+        for (int i=0; i<6; i++){
+            Assert.assertNotEquals(sliced.get(i), intSLL.get(i+2));
+        }
+
+        sliced = intSLL.slice(2,12);//indexOutOfBounds, return an empty list
+        Assert.assertNull(sliced.get(0));
+        Assert.assertEquals(0, sliced.size());
+
+        sliced = intSLL.slice(-1,8);//indexOutOfBounds, return an empty list
+        Assert.assertNull(sliced.get(0));
+        Assert.assertEquals(0, sliced.size());
+
+        sliced = sliced.slice(0,8);//indexOutOfBounds, return an empty list
+        Assert.assertNull(sliced.get(0));
+        Assert.assertEquals(0, sliced.size());
+
+        sliced = intSLL.slice(2,2);//impossible index combination
+        Assert.assertNull(sliced.get(0));
+        Assert.assertEquals(0, sliced.size());
+
+        sliced = intSLL.slice(3,2);//impossible index combination
+        Assert.assertNull(sliced.get(0));
+        Assert.assertEquals(0, sliced.size());
+
+    }
 
 }
