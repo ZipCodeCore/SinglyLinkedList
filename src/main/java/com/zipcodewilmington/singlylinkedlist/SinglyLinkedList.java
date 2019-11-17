52aaba85dbfeed556ccfcdb567051be1d01ed1d2
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-11-17 15:44:33 -0500

    added methods to start

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,4 +4,39 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+
+
+    public void add(){
+
+    }
+
+    public void remove(){
+
+    }
+
+    public void contains(){
+
+    }
+
+    public void find(){
+
+    }
+
+    public void size(){
+
+    }
+
+    public void get(){
+
+    }
+
+    public void copy(){
+
+    }
+
+    public void sort(){
+
+    }
+
+
 }
