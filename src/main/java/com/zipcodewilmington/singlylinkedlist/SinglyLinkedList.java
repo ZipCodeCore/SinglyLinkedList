65f4f99c2583e31aa286e3921e4000cca4a5ac9b
kshields412 <kshields412@gmail.com>
2019-11-18 10:50:14 -0500

    checkpoint

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,21 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+    Node head = null;
+
+    public class Node{
+        int data;
+        Node next;
+
+        public Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
 }
