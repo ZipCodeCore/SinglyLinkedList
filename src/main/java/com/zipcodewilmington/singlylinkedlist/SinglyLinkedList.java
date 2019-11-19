5d5860318730c90dc50518656ab5328f1b21a7b8
kshields412 <kshields412@gmail.com>
2019-11-18 23:14:04 -0500

    completed lab

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,21 +1,119 @@
 package com.zipcodewilmington.singlylinkedlist;
 
 
-
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
     Node head = null;
 
-    public class Node{
+    static class Node {
         int data;
         Node next;
 
-        public Node(int d) {
+        Node(int d) {
             data = d;
-            next = null;
         }
     }
 
+    public void add(int data) {
+        Node nh = head;
+        if (nh == null) {
+            head = new Node(data);
+        } else {
+            while (nh.next != null) {
+                nh = nh.next;
+            }
+            nh.next = new Node(data);
+        }
+    }
+
+    public void remove(int data) {
+        Node nh = head;
+        if (nh.data == data) {
+            head = head.next;
+        }
+        while (nh.next != null) {
+            if (nh.next.data == data) {
+                nh.next = nh.next.next;
+                return;
+            }
+            nh = nh.next;
+        }
+    }
+
+    public Boolean contains(int data) {
+        Node nh = head;
+        while(nh != null) {
+            if(nh.data == data) {
+                return true;
+            }
+            nh = nh.next;
+        }
+        return false;
+    }
+
+    public int find(int data){
+        Node nh = head;
+        int currIndex = 1;
+        while(nh != null){
+            if(nh.data == data){
+                return currIndex;
+            }
+            currIndex++;
+            nh = nh.next;
+        }
+        return currIndex;
+    }
+
+    public int size(){
+        Node nh = head;
+        int sizeOfNode = 0;
+        while (nh != null){
+            sizeOfNode++;
+            nh = nh.next;
+        }
+        return sizeOfNode;
+    }
+
+    public int get(int index){
+        Node nh = head;
+        int currIndex = 1;
+        while (nh != null){
+            if(currIndex == index){
+                return nh.data;
+            }
+            currIndex++;
+            nh = nh.next;
+        }
+        return currIndex;
+    }
+
+    public SinglyLinkedList copy(){
+        SinglyLinkedList sllCopy = new SinglyLinkedList();
+        Node nh = head;
+        while(nh != null){
+            sllCopy.add(nh.data);
+            nh = nh.next;
+        }
+        return sllCopy;
+    }
+
+    public void sort() {
+        Node nh = head, index;
+        int temp;
+        while(nh != null){
+            index = nh.next;
+            while(index != null){
+                if (nh.data > index.data){
+                    temp = nh.data;
+                    nh.data = index.data;
+                    index.data = temp;
+                }
+                index = index.next;
+            }
+            nh = nh.next;
+        }
+    }
+//https://stackoverflow.com/questions/16033800/bubble-sort-implementation-on-linked-lists bubble sort
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,96 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    SinglyLinkedList sll;
+
+    @Before
+    public void before(){
+        sll = new SinglyLinkedList();
+        sll.add(1);
+        sll.add(10);
+        sll.add(5);
+        sll.add(3);
+        sll.add(15);
+    }
+
+    @Test
+    public void testAdd(){
+        sll.add(11);
+        Assert.assertTrue(sll.contains(11));
+    }
+
+    @Test
+    public void testRemove(){
+        sll.remove(10);
+        Assert.assertFalse(sll.contains(10));
+    }
+
+    @Test
+    public void testContains(){
+        Assert.assertTrue(sll.contains(3));
+    }
+
+    @Test
+    public void testFind(){
+        Assert.assertEquals(2, sll.find(10));
+        Assert.assertEquals(5, sll.find(15));
+        Assert.assertEquals(1, sll.find(1));
+    }
+
+    @Test
+    public void testSize(){
+        Assert.assertEquals(5, sll.size());
+    }
+
+    @Test
+    public void testSizeAndAdd(){
+        sll.add(6);
+        sll.add(18);
+
+        Assert.assertEquals(7, sll.size());
+    }
+
+    @Test
+    public void testGet(){
+        Assert.assertEquals(10, sll.get(2));
+        Assert.assertEquals(15, sll.get(5));
+        Assert.assertEquals(1, sll.get(1));
+    }
+
+    @Test
+    public void testCopy1(){
+        SinglyLinkedList sllCopy = new SinglyLinkedList();
+        sllCopy.copy();
+        Assert.assertEquals(1, sllCopy.find(10));
+    }
+    @Test
+    public void testCopy2(){
+        SinglyLinkedList sllCopy = new SinglyLinkedList();
+
+        sllCopy.add(1);
+        sllCopy.add(10);
+        sllCopy.add(5);
+        sllCopy.add(3);
+        sllCopy.add(15);
+
+        Assert.assertEquals(2, sllCopy.find(10));
+        Assert.assertEquals(10, sllCopy.get(2));
+        Assert.assertNotEquals(2, sllCopy.get(15));
+        Assert.assertNotEquals(10, sllCopy.get(3));
+    }
+
+    @Test
+    public void testSort(){
+        sll.sort();
+        Assert.assertEquals(1, sll.get(1));
+        Assert.assertEquals(3, sll.get(2));
+        Assert.assertEquals(5, sll.get(3));
+    }
 }
