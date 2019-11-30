8e18f2f6fb9e157bc909b6287c91a1d537000743
vaidehi4321 <56602405+vaidehi4321@users.noreply.github.com>
2019-11-30 11:39:44 -0500

    singly linkedList

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -5,48 +5,195 @@ import java.util.LinkedList;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
-    //represents a node of the singly linked list
-    class Node {
-        int data;
-        Node next;
+public class SinglyLinkedList<AnyType> implements Cloneable {
+    public class Node<AnyType> {
 
-        public Node(int data) {
+
+        AnyType data;
+        Node<AnyType> next;
+
+        public Node(AnyType data) {
             this.data = data;
             this.next = null;
         }
-    }
 
-    public Node head = null;
-    public Node tail = null;
+        public AnyType getData() {
+            return data;
+        }
 
-    //addNode will add node to a list
-    public void addNode(int data) {
-        //create a new node
-        Node newNode = new Node(data);
+        public Node<AnyType> getNext() {
+            return next;
+        }
 
-        //checks if the list is empty
-        if (head == null) {
-            //if the list is empty both head and tail will point to the same node
-            head = newNode;
-            tail = newNode;
-        } else {
-            //new node will be added after a tail such that tail's next will be point to newNode
-            tail.next = newNode;
-            //new node will become a new tail of the list
-            tail = newNode;
+
+        public void setNext(Node<AnyType> next) {
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" + "data = " + data + ", next = " + next + "}";
         }
 
     }
-LinkedList list=new LinkedList();
-    public void add(Node node){
-        list.add(node);
+    private Node<AnyType> head=null;
+    private Node<AnyType> tail;
+    private Integer size;
 
+    public SinglyLinkedList(AnyType data) {
+        this.head = new Node<AnyType>(data);
+
+        this.size = 0;
     }
-    public void remove(Node node){
-        list.remove(node);
+    public SinglyLinkedList(){
+        this.size =0;
+    }
+    public void add(AnyType data){
+
+
+        if(head != null){
+            Node current = head;
+
+            while(current != null){
+                if(current.getNext() == null){
+                    current.setNext(new Node<AnyType>(data));
+                    size++;
+                    break;
+                }
+                current =current.getNext();
+
+            }
+
+        }
+        else
+        {
+            this.head = new Node<AnyType>(data);
+            size++;
+        }
     }
 
+    public Boolean remove(Integer index){
+        Node current = this.head;
+        Node previous = null;
+        Integer counter = 0;
+
+        while(current != null){
+            if(index.equals(0)){
+                this.head = current.getNext();
+                size--;
+                return true;
+            }
+            else if(counter == index - 1){
+
+                previous.setNext(current.getNext());
+                size--;
+                return true;
+            }
+            counter++;
+            previous = current;
+            current = current.getNext();
+        }
+        return false;
+    }
+    public Boolean contains(AnyType data){
+        Node current = this.head;
+
+        while(current != null){
+            if(current.getData() == data|| current.getNext() == data){
+                return true;
+            }
+            current = current.getNext();
+        }
+        return false;
+    }
+
+    public Integer find(AnyType data){
+        Node current = this.head;
+        Integer index = 1;
+
+        while(current != null){
+            if(current.getData() == data){
+                return index;
+            }
+            index++;
+            current = current.getNext();
+        }
+
+        return -1;
+    }
+
+    public Object getByIndex(Integer index) {
+
+        Node current = this.head;
+        Integer currentIndex = 1;
+
+        while(current != null){
+            if(currentIndex.equals(index))
+                return current.getData();
+
+            current = current.getNext();
+            currentIndex++;
+        }
+
+        return null;
+    }
+
+    public Node reverse(Node node){
+        Node previous = null;
+        Node current = this.head;
+        Node next = current;
+
+        while(current != null){
+            next = next.getNext();
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        return previous;
+    }
+
+    protected Object clone() throws CloneNotSupportedException{
+        return super.clone();
+    }
+
+    public Node getTail(){
+        Node current = this.head;
+
+        while(current != null){
+            if(current.getNext() == null){
+                return current;
+            }
+            current = current.getNext();
+        }
+        return null;
+    }
+
+    public Node sort(){
+        return null;
+    }
+
+
+
+    public Node<AnyType> getHead() {
+        return head;
+    }
+
+    public void setHead(Node<AnyType> head) {
+        this.head = head;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "SinglyLinkedList{" +
+                "head=" + head +
+                ", size=" + size +
+                '}';
+    }
 
 }
 
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,25 +1,178 @@
 package com.zipcodewilmington.singlylinkedlist;
-
+import org.junit.Assert;
+import org.junit.Test;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedListTest {
+public class SinglyLinkedListTest<AnyType> {
+    @Test
+    public void ConstructorTest(){
 
-    //display() will show all the nodes present in the list
-//    public void display(){
-//        Node current = head ;
-//        if(head==null){
-//            System.out.println("list is empty");
-//            return;
-//        }
-//        System.out.println("Noes of singly linked list: ");
-//        while(current!=null){
-//            //prints each node by incrementing pointer
-//            System.out.println(current.data +"");
-//            current=current.next;
+        String expected = "Sonia";
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<String>(expected);
 
-    //SinglyLinkedList sList = new SinglyLinkedList();
- //   sList.addNode()
-//
+        Assert.assertEquals(expected, linkedList.getHead().getData());
+    }
+
+    @Test
+    public void testAdd(){
+        //given
+        SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<Integer>();
+
+        //when
+        linkedList.add(10);
+        linkedList.add(20);
+        linkedList.add(50);
+        linkedList.add(70);
+        Integer expected = 4;
+
+        //then
+        Assert.assertEquals(expected, linkedList.getSize());
+
+    }
+
+    @Test
+    public void testRemove(){
+        //given
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<String>();
+        linkedList.add("Sonia");
+        linkedList.add("Rajani");
+        linkedList.add("Veer");
+
+        //when
+        Boolean remove = linkedList.remove(2);
+
+        //then
+        Assert.assertTrue(remove);
+
+    }
+
+    @Test
+    public void testRemove2(){
+        //given
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<String>();
+        linkedList.add("Sonia");
+        linkedList.add("Rajani");
+        linkedList.add("Veer");
+
+        //when
+        linkedList.remove(2);
+
+        //then
+        Assert.assertFalse(linkedList.contains("Rajani"));
+    }
+
+    @Test
+    public void testContain(){
+        //given
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<String>();
+
+        //when
+        linkedList.add("Sonia");
+        linkedList.add("Rajani");
+        linkedList.add("Veer");
+        linkedList.add("Rashmi");
+        System.out.println(linkedList.toString());
+        //then
+        Assert.assertTrue(linkedList.contains("Sonia"));
+        Assert.assertTrue(linkedList.contains("Rajani"));
+        Assert.assertTrue(linkedList.contains("Veer"));
+
+    }
+
+    @Test
+    public void testFind(){
+        //given
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<String>();
+
+        //when
+        linkedList.add("Sonia");
+        linkedList.add("Rajani");
+        linkedList.add("Veer");
+
+        //then
+        Assert.assertEquals((Integer)1, linkedList.find("Sonia"));
+        Assert.assertEquals((Integer)3, linkedList.find("Veer"));
+    }
+
+    @Test
+    public void testSize(){
+        //given
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<String>();
+
+        //when
+        linkedList.add("Sonia");
+        linkedList.add("Rajani");
+        linkedList.add("Veer");
+        Integer expected = 3;
+
+        //then
+        Assert.assertEquals(expected, linkedList.getSize());
+    }
+
+    @Test
+    public void testGetByIndex(){
+        //given
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<String>();
+
+        //when
+        linkedList.add("Sonia");
+        linkedList.add("Rajani");
+        linkedList.add("Veer");
+
+        Assert.assertEquals("Rajani", linkedList.getByIndex(2));
+    }
+
+    @Test
+    public void testClone(){
+        //given
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<String>();
+        linkedList.add("Sonia");
+        linkedList.add("Rajani");
+        linkedList.add("Veer");
+
+        //when
+        SinglyLinkedList<String> linkedList1 = null;
+        try{
+            linkedList1 = (SinglyLinkedList<String>) linkedList.clone();
+        }catch(CloneNotSupportedException e){
+            e.printStackTrace();
+        }
+
+        //then
+        Assert.assertEquals(linkedList.toString(), linkedList1.toString());
+
+
+    }
+    ///Check the reverse method<<<<<<<<<<
+    @Test
+    public void testReverse(){
+        //given
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<String>();
+        linkedList.add("Sonia");
+        linkedList.add("Rajani");
+        linkedList.add("Veer");
+
+
+
+        //then
+        Assert.assertEquals("Veer", linkedList.reverse(linkedList.getHead()).getData());
+    }
+
+    @Test
+    public void testSort(){
+        //given
+        SinglyLinkedList<String> linkedList = new SinglyLinkedList<String>();
+        linkedList.add("Sonia");
+        linkedList.add("Rajani");
+        linkedList.add("Veer");
+
+        System.out.println(linkedList.getHead().toString());
+
+        linkedList.sort();
+
+        System.out.println(linkedList.getHead().toString());
+    }
+
 
     }
