b8d420a4eff50a8b399be125f895779cf1f61e07
jgiroso <jrgiroso@gmail.com>
2021-07-25 15:47:33 -0400

    methods in progress. working on fixing the find method

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,79 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+
+
+    class Node{
+        T data;
+        Node next;
+
+        public Node(T data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node head = null;
+    public Node tail = null;
+
+    public void add(T data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+        tail = newNode;
+    }
+
+    public int size() {
+        Node temp = head;
+        int count = 0;
+        while(temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    public Integer find(T data) {
+        Node temp = head;
+        for(int i = 0; i < size(); i++) {
+            if(!temp.equals(data)) {
+                temp = temp.next;
+            } else return i;
+        }
+        return null;
+    }
+
+    public boolean contains(T data) {
+        Node current = head;
+        while (current != null) {
+            if(current.data == data) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public void remove(Integer index) {
+        //iterate by index through list
+        //if the index equals the value we're looking for, assign each value after that to the next
+        Node current = head;
+        Node last = head;
+        for (int i = 0; i < size(); i++) {
+             if(i != index) {
+                 last = current;
+                 current = current.next;
+             } else {
+                if(i == index) {
+                    while(i < size() - 1) {
+                        last.next = current.next;
+                    }
+                }
+             }
+        }
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,82 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void testAdd() {
+        //given
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+        Integer expected = 3;
+        //when
+        list.add(3);
+        list.add(5);
+        list.add(7);
+        Integer actual = list.size();
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSize() {
+        //given
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+        Integer expected = 3;
+        //when
+        list.add(3);
+        list.add(5);
+        list.add(7);
+        Integer actual = list.size();
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFind() {
+        //given
+        SinglyLinkedList<String> list = new SinglyLinkedList<String>();
+        Integer expected = 1;
+        //when
+        list.add("wine");
+        list.add("coffee");
+        list.add("tea");
+        Integer actual = list.find("coffee");
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testContains() {
+        //given
+        SinglyLinkedList<String> list = new SinglyLinkedList<String>();
+        //when
+        list.add("wine");
+        list.add("coffee");
+        list.add("tea");
+        //then
+        Assert.assertTrue(list.contains("tea"));
+    }
+
+    @Test
+    public void testRemove() {
+        //given
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+        //when
+        list.add(3);
+        list.add(5);
+        list.add(7);
+        list.add(2);
+        list.remove(2);
+        //then
+        Assert.assertTrue(list.contains(5));
+        Assert.assertFalse(list.contains(7));
+    }
+
 }
