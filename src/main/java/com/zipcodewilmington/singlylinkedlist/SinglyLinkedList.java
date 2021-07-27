128a6db7ddf3ec49d1f9c3feca5bdf0d94a0d3c3
MannyMb <emmanuelnefo98@gmail.com>
2021-07-27 02:33:18 -0400

    SAving progress

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,4 +4,88 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+    class Node {
+        Object data;
+        Node next;
+
+        public Node(Object data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    public Node head = null;
+    public Node tail = null;
+
+    public void add(Object data) {
+        Node newNode = new Node(data);
+        if(head == null) {
+            head = newNode;
+
+        } else {
+            tail.next = newNode;
+        }
+        tail = newNode;
+    }
+
+    public void remove(Object data) {
+        if (head == data || head.next == data) {
+            head = null;
+        } else if (tail == data) {
+            tail = null;
+        }
+    }
+
+    public boolean contains(Object data) {
+        if(head == data || head.next == data || tail == data) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Integer find(Object data) {
+
+        return null;
+    }
+
 }
+
+
+
+
+//<SomeType extends Comparable<SomeType>>{
+
+
+//    class Node {
+//        SomeType data;
+//        Node next;
+//
+//        public Node(SomeType data) {
+//            this.data = data;
+//            this.next = null;
+//        }
+//    }
+//
+//    public Node head = null;
+//    public Node tail = null;
+//
+//    public void add(SomeType data) {
+//        Node newNode = new Node(data);
+//        if(head == null) {
+//            head = newNode;
+//
+//        } else {
+//            tail.next = newNode;
+//        }
+//        tail = newNode;
+//    }
+//
+//    public void remove(SomeType data) {
+//        Node newNode = new Node(data);
+//
+//    }
+
+
+
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,93 @@
 package com.zipcodewilmington.singlylinkedlist;
+import org.junit.Assert;
+import org.junit.Test;
+
+
 
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void testAdd() {
+        // Given
+        SinglyLinkedList singlyLinkedList1 = new SinglyLinkedList();
+
+        // When
+        singlyLinkedList1.add(10);
+
+        // Then
+        Assert.assertEquals(singlyLinkedList1, 10);
+    }
+
+    @Test
+    public void testRemove() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+    @Test
+    public void testContains() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+    @Test
+    public void testFind() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+    @Test
+    public void testSize() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+    @Test
+    public void testGet() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+    @Test
+    public void testCopy() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+    @Test
+    public void testSort() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
 }
