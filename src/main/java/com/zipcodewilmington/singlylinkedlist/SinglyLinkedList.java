307b99bd84aec815484249d99d8dd09ceee6dead
MannyMb <emmanuelnefo98@gmail.com>
2021-07-27 23:09:24 -0400

    saving progress

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+
+
 /**
  * Created by leon on 1/10/18.
  */
@@ -20,7 +22,7 @@ public class SinglyLinkedList {
 
     public void add(Object data) {
         Node newNode = new Node(data);
-        if(head == null) {
+        if (head == null) {
             head = newNode;
 
         } else {
@@ -29,30 +31,97 @@ public class SinglyLinkedList {
         tail = newNode;
     }
 
-    public void remove(Object data) {
-        if (head == data || head.next == data) {
-            head = null;
-        } else if (tail == data) {
-            tail = null;
+    public Object remove(Integer index) {  // REFACTOR TO RETURN TYPE VOID
+        SinglyLinkedList list = new SinglyLinkedList();
+        Node current = list.head;
+        Node previous = null;
+        if (index == 0 && current != null) {
+            list.head = current.next;
+            return list;
         }
+        int counter = 0;
+        while(current != null) {
+            if(counter == index) {
+                previous.next = current.next;
+                break;
+            } else {
+                previous = current;
+                current = current.next;
+                counter++;
+            }
+        }
+        if (current == null) {
+            return -1;
+        }
+        return list;
     }
 
-    public boolean contains(Object data) {
-        if(head == data || head.next == data || tail == data) {
-            return true;
+
+    public Boolean contains(Object data) {
+        Node current = head;
+        while (current != null) {
+            if (current.data == data){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public Object find(Object data) {  // NOT WORKING
+        Node current = head;
+        int index = 1;
+        boolean flag = false;
+        if (head == null) {
+            return -1;
         } else {
-            return false;
+            while (current != null) {
+                if (current.data == data) {
+                    flag = true;
+                }
+                index++;
+                current = current.next;
+            }
         }
+        if (flag) {
+            return index;
+        }
+        return -1;
     }
 
-    public Integer find(Object data) {
 
+    public Integer size() {
+        Integer counter = 0;
+        Node current = head;
+        while (current != null) {
+            counter++;
+            current = current.next;
+        }
+        return counter;
+    }
+
+    public Object get(Integer index) {
+        Node current = head;
+        int counter = 0;
+        while(current != null) {
+            if (counter == index) {
+                return current.data;
+            }
+            counter++;
+            current = current.next;
+        }
+        return -1;
+    }
+
+    public SinglyLinkedList copy() {
         return null;
     }
 
+    public void sort() {
+
+    }
+
 }
-
-
 
 
 //<SomeType extends Comparable<SomeType>>{
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -12,62 +12,85 @@ public class SinglyLinkedListTest {
     @Test
     public void testAdd() {
         // Given
-        SinglyLinkedList singlyLinkedList1 = new SinglyLinkedList();
-
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.add(1);
+        singlyLinkedList.add(2);
+        Integer expected = 2;
         // When
-        singlyLinkedList1.add(10);
+        Integer actual = singlyLinkedList.size();
 
         // Then
-        Assert.assertEquals(singlyLinkedList1, 10);
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testRemove() {
         // Given
-
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.add(1);
+        singlyLinkedList.add(2);
         // When
-
+        singlyLinkedList.remove(2);
+        Integer expectedSize = 1;
+        Integer actualSize = singlyLinkedList.size();
         // Then
-
+        Assert.assertEquals(expectedSize,actualSize);
     }
 
     @Test
     public void testContains() {
         // Given
-
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.add("one");
+        singlyLinkedList.add("two");
+        Boolean expected = true;
         // When
-
+        Boolean actual = singlyLinkedList.contains("one");
         // Then
-
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testFind() {
         // Given
-
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.add("one");
+        singlyLinkedList.add("two");
+        singlyLinkedList.add("three");
+        Integer expectedIndex = 1;
         // When
-
+        Object actualIndex = singlyLinkedList.find("three");
         // Then
-
+        Assert.assertEquals(expectedIndex,actualIndex);
     }
 
     @Test
     public void testSize() {
         // Given
-
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.add(1);
+        singlyLinkedList.add(2);
         // When
-
+        Integer expectedSize = 2;
+        Integer actualSize = singlyLinkedList.size();
         // Then
-
+        Assert.assertEquals(expectedSize,actualSize);
     }
 
     @Test
     public void testGet() {
         // Given
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.add(1);
+        singlyLinkedList.add(2);
+        singlyLinkedList.add(3);
 
         // When
+        Integer expectedElement = 2;
+        Integer actualElement = (Integer) singlyLinkedList.get(1);
 
         // Then
+        Assert.assertEquals(expectedElement, actualElement);
 
     }
 
