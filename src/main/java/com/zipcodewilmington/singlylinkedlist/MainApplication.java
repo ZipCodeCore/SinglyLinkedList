6f150fa8d45d8c9d59a48e33e801622a72b02eff
junior <junior@zipcodes-MacBook-Pro-5.local>
2021-11-25 16:39:00 -0500

    added add method, size method, find method and contains method

--- src/main/java/com/zipcodewilmington/singlylinkedlist/MainApplication.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/MainApplication.java
@@ -4,4 +4,17 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class MainApplication {
+
+    public static void main(String[] args) {
+        SinglyLinkedList<String> singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.add("1");
+        singlyLinkedList.add("2");
+        singlyLinkedList.add("3");
+
+
+        Boolean result = singlyLinkedList.contains("4");
+        System.out.println(result);
+        System.out.println(singlyLinkedList.find("1"));
+    }
+
 }
--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,77 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+
+import jdk.nashorn.internal.ir.WhileNode;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T extends Comparable<T>> {
+
+    public SinglyLinkedList() {
+    }
+
+    class Node<T> {
+        T data;
+        Node next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node head = null;
+    public Node tail = null;
+
+
+    public void add(T data) {
+        Node node = new Node(data);
+        if (head == null) {
+            head = node;
+        } else {
+            tail.next = node;
+        }
+        tail = node;
+    }
+
+
+    public Integer size() {
+        Node current = head;
+        Integer counter = 0;
+        while (current != null) {
+            counter++;
+            current = current.next;
+
+        }
+        return counter;
+    }
+
+    public Integer find(T data) {
+        Node current = head;
+        Integer index = 0;
+
+        while (current != null) {
+            if (current.data == data) {
+                return index;
+            }
+            index++;
+            current = current.next;
+        }
+        return -1;
+    }
+
+    public Boolean contains(T data) {
+        Node current = head;
+
+        while (current != null) {
+            if (current.data == data) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+
 }
